30c6aa6e3a800741ce9b4805fad38ad0392d8a3b
gp03 <g@gmailzzz.comzzz>
2021-03-09 18:22:41 +0800

    Add skip files. Set interval to 15min

--- src/libs/file-cleaner.ts
+++ src/libs/file-cleaner.ts
@@ -7,6 +7,15 @@ const run = async (dirName = 'uploads') => {
   for (let f of files) {
     const fileWithDir = `${dirName}/${f}`;
     const stat = fs.statSync(fileWithDir);
+
+    /**
+     * skip directory and script-*.js
+     */
+    if (!stat.isFile || f.indexOf('script-') !== -1) {
+      console.log('[filter-cleaner] skip file', f);
+      continue;
+    }
+
     const diff = +now - +stat.atime;
     if (diff >= 1000 * 60 * 60 * 24) {
       console.warn('[filter-cleaner] delete file', f);
@@ -18,7 +27,7 @@ const run = async (dirName = 'uploads') => {
 
 const startAsService = async () => {
   await run();
-  setInterval(run, 1000 * 60 * 60);
+  setInterval(run, 1000 * 60 * 15); // 15m
 };
 
 export default {
