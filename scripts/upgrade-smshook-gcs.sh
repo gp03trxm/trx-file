2a06f1dd71eb71136d1c9b89e3a328381ead8f46
gp03 <g@gmailzzz.comzzz>
2021-03-09 15:25:15 +0800

    Add new site and new host

--- scripts/upgrade-smshook-gcs.sh
+++ scripts/upgrade-smshook-gcs.sh
@@ -1,5 +1,5 @@
-: ${VERSION="87"}
-: ${HOSTS="file.vnpay.io file.trxm.cc file.defigo.vc"}
+: ${VERSION="111"}
+: ${HOSTS="file.vnpay.io file.trxm.cc file.defigo.vc file.saokim.io file1.wbcn.one"}
 
 # convert string to array by space
 HOSTS=($HOSTS)
@@ -10,8 +10,12 @@ LOCAL_VN_APK="`pwd`/tmp-smshook-${VERSION}-vn-release.apk"
 GCS_DFG_APK="tl-dfg/apks/smshook-${VERSION}-dfg-release.apk"
 LOCAL_DFG_APK="`pwd`/tmp-smshook-${VERSION}-dfg-release.apk"
 
+GCS_CN_APK="tl-trx/cn/smshook-${VERSION}-cn-release.apk"
+LOCAL_CN_APK="`pwd`/tmp-smshook-${VERSION}-cn-release.apk"
+
 gsutil -h "Cache-Control:no-cache,max-age=0" cp gs://$GCS_VN_APK $LOCAL_VN_APK
 gsutil -h "Cache-Control:no-cache,max-age=0" cp gs://$GCS_DFG_APK $LOCAL_DFG_APK
+gsutil -h "Cache-Control:no-cache,max-age=0" cp gs://$GCS_CN_APK $LOCAL_CN_APK
 
 for host in "${HOSTS[@]}":
   do
@@ -35,4 +39,11 @@ for host in "${HOSTS[@]}":
     --form "file=@$LOCAL_DFG_APK" \
     --form 'appId=trx.utils.smshook#dfg#release' \
     --form "version=${VERSION}" | jq ".apps.\"trx.utils.smshook#dfg#release\".versions.\"${VERSION}\""
+
+    # for cn
+    curl --location --request POST "https://${host}/app-upgrade" \
+    --header 'Content-Type: multipart/form-data' \
+    --form "file=@$LOCAL_CN_APK" \
+    --form 'appId=trx.utils.smshook#cn#release' \
+    --form "version=${VERSION}" | jq ".apps.\"trx.utils.smshook#cn#release\".versions.\"${VERSION}\""
   done
